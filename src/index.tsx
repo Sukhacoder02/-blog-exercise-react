3711127fffa5c8a1ee4798e2b1ec634cb79a0d9b
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-02-16 16:45:15 +0530

    chore: add index.html and styles.css in public

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Navbar } from './components';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>
-);
+root.render(<Navbar />);
