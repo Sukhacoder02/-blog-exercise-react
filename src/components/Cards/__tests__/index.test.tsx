759e21cd82f02d9f82ab7a2bb7a76c48e5517264
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-02-20 23:43:02 +0530

    test: add testcases for cards component

--- src/components/Cards/__tests__/index.test.tsx
+++ src/components/Cards/__tests__/index.test.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import Cards from '..';
+import posts from '../../../mockData/index.json';
 
 describe('Component Cards', () => {
   it('renders without crashing', () => {
     const { asFragment } = render(<Cards />);
     expect(asFragment()).toMatchSnapshot();
+  });
+  it(`Should contain ${posts.length} cards`, () => {
+    const { getAllByTestId } = render(<Cards />);
+    const cards = getAllByTestId('card');
+    expect(cards.length).toEqual(posts.length);
   });
 });
