6e3e889f1b6d9fbdbb3dee17f27764c4ae7fadb8
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-02-22 17:32:58 +0530

    test: update testcases for MainDiv component

--- src/components/MainDiv/__tests__/index.test.tsx
+++ src/components/MainDiv/__tests__/index.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import MainDiv from '..';
 
+jest.mock('react-router-dom');
 describe('Component MainDiv', () => {
   it('Should render without crashing', () => {
     const { asFragment } = render(<MainDiv />);
